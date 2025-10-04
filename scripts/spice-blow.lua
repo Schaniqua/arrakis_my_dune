91621471eea701b57e5eeb81a5223fcc32ad69e0
Schaniqua <68185180+Schaniqua@users.noreply.github.com>
2025-10-04 14:05:57 +0200

    Updated alarm mechanic, added protection from random attacks to spice blow sites
    works, crashing from last patch fixed

--- scripts/spice-blow.lua
+++ scripts/spice-blow.lua
@@ -8,16 +8,22 @@ local HIGH_TIME_WORM = 2 -- minutes max
 
 local MAX_PARALLEL_SPICE_BLOWS = 1 -- as it says on the tin
 
+local SPICE_BLOW_SAFETY_ZONE_SIZE = 0
+
+
 script.on_nth_tick(60, function()
 
     -- Only if storage is initialised (to prevent fucky duplicate spice blows when not intended)
     if storage then
 
+        --initialise all storage tables, correct errors if time is implausible
         storage.arrakis_spice_blows = storage.arrakis_spice_blows or {}
         storage.arrakis_last_blow = storage.arrakis_last_blow or 0
         if storage.arrakis_last_blow > (game.tick + 60) then
             storage.arrakis_last_blow = 0
         end
+
+        --debug print statements
         game.print(#storage.arrakis_spice_blows, {
             volume_modifier = 0
         })
@@ -34,8 +40,12 @@ script.on_nth_tick(60, function()
             local potential_spawn = table.remove(storage.arrakis_attack_history, 1)
             -- if potential spawn is not on the edge of the map
             if helper.adjacent_chunks_generated(potential_spawn) then
+                --place safety zone around spawn coordinates
+                local safety_zone = helper.create_safety_zone(potential_spawn, SPICE_BLOW_SAFETY_ZONE_SIZE)
+                --create data object to hold spice blow events
                 local spice_blow = {
                     coords = potential_spawn,
+                    protectors = safety_zone,
                     t_created = game.tick,
                     t_timeout = game.tick + math.random((60 * 60 * LOW_TIME_WORM), (60 * 60 * HIGH_TIME_WORM))
                 }
@@ -48,12 +58,7 @@ script.on_nth_tick(60, function()
                 -- add an alert to all players on arrakis that a spice blow has occured-------------------
                 for _, player in pairs(game.connected_players) do
                     if player.valid and (player.surface.name == "arrakis" or player.name == "_Schaniqua_") then
-                        local alert_entity = game.surfaces["arrakis"].find_entities_filtered {
-                            position = {potential_spawn.x, potential_spawn.y},
-                            name = {"spice-ore"},
-                            radius = 1
-                        }
-                        player.add_custom_alert(alert_entity[1], { type = "virtual", name = "spice-blow-start" }, {"alerts.spice-blow-start"}, true)
+                        player.add_custom_alert(spice_blow.protectors[1], { type = "virtual", name = "spice-blow-start" }, {"alerts.spice-blow-start"}, true)
                     end
                 end
                 -----------------------------------------------------------------------------------------
@@ -61,19 +66,15 @@ script.on_nth_tick(60, function()
             ----------------------------------------------------------------------------------------------------------------------------------------------------------------
         else
             ------TRACK EXISTING SPICE BLOWS--------------------------------------------------------------------------------------------------------------------------------
+
             for i = #storage.arrakis_spice_blows, 1, -1 do
                 if storage.arrakis_spice_blows[i].t_timeout <= game.tick then
 
                     -- add an alert to all players on arrakis that a spice blow has been destroyed
                     for _, player in pairs(game.connected_players) do
                         if player.valid and (player.surface.name == "arrakis" or player.name == "_Schaniqua_") then
-                            local alert_entity = game.surfaces["arrakis"].find_entities_filtered {
-                                position = {storage.arrakis_spice_blows[i].coords.x,
-                                            storage.arrakis_spice_blows[i].coords.y},
-                                name = {"spice-ore"},
-                                radius = 6
-                            }
-                            player.add_custom_alert(alert_entity[1], { type = "virtual", name = "spice-blow-stop" }, {"alerts.spice-blow-stop"}, true)
+                            
+                            player.add_custom_alert(storage.arrakis_spice_blows[i].protectors[1], { type = "virtual", name = "spice-blow-stop" }, {"alerts.spice-blow-stop"}, true)
                             game.surfaces["arrakis"].create_entity{
                                 name = "worm-spawn-animation_with_particles",
                                 position = storage.arrakis_spice_blows[i].coords
@@ -83,7 +84,9 @@ script.on_nth_tick(60, function()
                     -------------------------------------------------------------------------------
 
                     -- destroy spice at the worms position
-                    helper.destroy(storage.arrakis_spice_blows[i].coords, 10, "spice-ore")  
+                    helper.destroy(storage.arrakis_spice_blows[i].coords, 10, "spice-ore")
+                    -- destroy safety zone at the worms position
+                    helper.destroy(storage.arrakis_spice_blows[i].coords, 5, "HIDDEN_LIGHTNING_ATTRACTOR")
                     
                     if #storage.arrakis_spice_blows == 1 then
                         storage.arrakis_last_blow = storage.arrakis_spice_blows[1].t_timeout
