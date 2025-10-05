f448308a976dfde3737f98dee84ab0130e42419c
Schaniqua <68185180+Schaniqua@users.noreply.github.com>
2025-10-05 17:49:05 +0200

    Added dynamic protection system for moving vehicles

--- control.lua
+++ control.lua
@@ -1,8 +1,14 @@
 require("scripts.init")
 require("scripts.humidity-generation")
-require("scripts.script-trigger-worm-targeting")
+
+
+-- seperate scripts for needed game mechanics
 require("scripts.deny-ice-from-orbit")
+
+-- seperate scripts for protecting different things
 require("scripts.protect-rock-islands")
+require("scripts.protect-vehicles")
 
-
+-- spice blow system
+require("scripts.script-trigger-worm-targeting")
 require("scripts.spice-blow")
---
+++ scripts/protect-vehicles.lua
@@ -0,0 +1,79 @@
+require("scripts.helper-functions")
+
+-- prototype names of protected vehicles
+local protected_vehicles = {
+    ["vehicle-miner"] = true,
+    ["vehicle-miner-mk2"] = true,
+    ["vehicle-miner-mk3"] = true,
+    ["vehicle-miner-mk4"] = true,
+    ["vehicle-miner-mk5"] = true,
+    ["vehicle-miner-0"] = true,
+    ["vehicle-miner-mk2-0"] = true,
+    ["vehicle-miner-mk3-0"] = true,
+    ["vehicle-miner-mk4-0"] = true,
+    ["vehicle-miner-mk5-0"] = true
+}
+
+local MIN_SPEED_FOR_TARGET = 0.01
+
+script.on_nth_tick(30, function()
+    -- if storage is initialised
+    if storage then
+
+        -- read or declare data object to hold vehicle lightning attractors
+        storage.vehicle_protectors = storage.vehicle_protectors or {}
+        -- iterate through all vehicles that need to be protected
+        for name, _ in pairs(protected_vehicles) do
+            -- find every instance of vehicle by name "name"
+            local vehicles = game.surfaces["arrakis"].find_entities_filtered {
+                name = name
+            }
+            -- for each existing vehicle on the surface of arrakis
+            for _, vehicle in ipairs(vehicles) do
+                -- if vehicle is valid data object
+                if vehicle.valid and vehicle.speed >= MIN_SPEED_FOR_TARGET then
+                    -- try to read if vehicle already has an entry in vehicle_protectors
+                    local entry = storage.vehicle_protectors[vehicle.unit_number]
+                    -- if vehicle has entry and entry is valid lightning attractor luaentity
+                    if entry and entry.valid then
+                        -- move existing lightning attractor with vehicle
+                        entry.teleport(vehicle.position)
+                    else
+                        -- create new lightning attractor if vehicle has no valid existing attractor
+                        game.print("neu")
+                        local new = game.surfaces["arrakis"].create_entity {
+                            name = "HIDDEN_LIGHTNING_ATTRACTOR",
+                            position = vehicle.position,
+                            force = vehicle.force
+                        }
+                        new.destructible = false
+                        -- assign storage entry to new lightning attractor attached to vehicle
+                        storage.vehicle_protectors[vehicle.unit_number] = new
+
+                    end
+                end
+            end
+        end
+
+        -- if storage entry exists but its ID doesnt correspond to a valid car / miner remove the lightning attractor and destroy the entry
+        -- also remove storage entry and lightning_attractor if speed is below MIN_SPEED_FOR_TARGET
+        for unit_number, entity in pairs(storage.vehicle_protectors) do
+
+            -- if referenced entity doesnt exist remove lightning attractor and entry
+            if not game.get_entity_by_unit_number(unit_number) then
+                storage.vehicle_protectors[unit_number].destroy()
+                storage.vehicle_protectors[unit_number] = nil
+            else
+                -- if referenced entity isnt above MIN_SPEED_FOR_TARGET movespeed then remove attractor but leave entry intact so we dont need to search next time
+                local unit_speed = game.get_entity_by_unit_number(unit_number).speed
+                if unit_speed < MIN_SPEED_FOR_TARGET then
+                    if storage.vehicle_protectors[unit_number].valid then
+                        storage.vehicle_protectors[unit_number].destroy()
+                        game.print("destroyer - current speed " .. unit_speed)
+                    end
+                end
+
+            end
+        end
+    end
+end)
